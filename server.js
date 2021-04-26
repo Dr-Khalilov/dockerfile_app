2fc34b36a989d4d1e14a1df5cba7c8fe3d8bf60a
Umar Khalilov <ERMASTER100@gmail.com>
2021-04-26 10:51:04 +0300

    add dockerized file

--- server.js
+++ server.js
@@ -2,10 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res) => {
-    res.send('Hello')
+app.get('/', (req, res) => {
+    res.send('Hello my Boss');
 });
+
+app.get('/test', (req, res) => {
+    res.send('test abarrakadabra');
+});
+
+app.get('/html', (req, res) => {
+    res.send(`
+<div>
+<h1>HELLO</h1>
+<p>test paragraph</p>
+</div>`);
+});
+
 
 const port = process.env.PORT || 3000;
 
-app.listen(port)
+app.listen(port);
